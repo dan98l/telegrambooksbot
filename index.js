1a41d0ccdd85ccfb44cc10538ae9632b7464e3cb
Daniiloooo <dan98l@mail.ru>
2018-01-05 17:07:31 +0700

    massegу 5

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Тимофей, привет! Круто, да?)))');
+  bot.sendMessage(chatId, 'Не-не-не, я жив!');
 });
