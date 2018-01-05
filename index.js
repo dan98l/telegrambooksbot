00e024030625a09c10f2622a11dc77627f7150bb
Daniiloooo <dan98l@mail.ru>
2018-01-05 16:39:48 +0700

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '517905898:AAFEpoBmcUzC68DCvtGp0wkECMP_sIbdWSM';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Привет! Это бот для чтения книг онлайн!');
+});
